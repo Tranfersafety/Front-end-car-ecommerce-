390968cf7c718021a9c4ee2bec9a67cdf4b4ebb7
Tranfersafety <143916026+Tranfersafety@users.noreply.github.com>
2024-07-17 13:36:52 +0700

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:5000/api";
-export const BASE_URL = "https://back-end-car-ecommerce-6.onrender.com";
+export const BASE_URL = "https://back-end-car-ecommerce-6.onrender.com/api";
 
 
 
